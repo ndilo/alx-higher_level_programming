6103fc50dda9173565999438c8b182267878b4fa
Brian Ndilo <b_munguti@yahoo.com>
2023-08-11 02:31:22 -0700

    Advanced Tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+from calculator_1 import add, sub, mul, div
+import sys
 
-    import sys
-    from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    operator = sys.argv[2]
-
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
-    elif operator == '/':
-        result = div(a, b)
-    else:
+    if sys.argv[2] not in "+-*/":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    print("{:d} {:s} {:d}".format(a, operator, b, result))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    print(f"{a} {sys.argv[2]} {b} = ", end="")
+    if sys.argv[2] == '+':
+        print(f"{add(a, b)}")
+    elif sys.argv[2] == '-':
+        print(f"{sub(a, b)}")
+    elif sys.argv[2] == '*':
+        print(f"{mul(a, b)}")
+    elif sys.argv[2] == '/':
+        print(f"{div(a, b)}")
