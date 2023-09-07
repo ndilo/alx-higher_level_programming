44fb123233ce5cf707eee720d5ae0e3e5af62c2f
Brian Ndilo <b_munguti@yahoo.com>
2023-09-07 15:09:38 -0700

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,27 +1,6 @@
 #!/usr/bin/python3
-"""
-Module multiplies 2 matrices
-Raise:
-    ValueError
-    TypeError
-"""
+"""Defines a matrix multiplication function."""
 def matrix_mul(m_a, m_b):
-    """
-    Multiplies two matrices m_a and m_b.
-
-    Args:
-        m_a (list of lists): The first matrix.
-        m_b (list of lists): The second matrix.
-
-    Returns:
-        list of lists: The result of multiplying m_a and m_b.
-
-    Raises:
-        TypeError: If m_a or m_b is not a list, not a list of lists, empty, or contains non-integer/float elements.
-                   If the rows of m_a or m_b are not of the same size.
-        ValueError: If m_a and m_b cannot be multiplied.
-
-    """
     # Validate m_a and m_b
     if not isinstance(m_a, list) or not isinstance(m_b, list):
         raise TypeError("m_a must be a list or m_b must be a list")
@@ -35,19 +14,19 @@ def matrix_mul(m_a, m_b):
     if not all(isinstance(num, (int, float)) for row in m_a for num in row) or not all(isinstance(num, (int, float)) for row in m_b for num in row):
         raise TypeError("m_a should contain only integers or floats or m_b should contain only integers or floats")
 
-    if len(set(len(row) for row in m_a)) != 1 or len(set(len(row) for row in m_b)) != 1:
-        raise TypeError("Each row of m_a must be of the same size or each row of m_b must be of the same size")
+    if not all(len(row) == len(m_a[0]) for row in m_a) or not all(len(row) == len(m_b[0]) for row in m_b):
+        raise ValueError("Each row of m_a must be of the same size or each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
-    # Perform matrix multiplication
+    # Initialize the result matrix with zeros
     result = [[0 for _ in range(len(m_b[0]))] for _ in range(len(m_a))]
 
+    # Perform matrix multiplication
     for i in range(len(m_a)):
         for j in range(len(m_b[0])):
             for k in range(len(m_b)):
                 result[i][j] += m_a[i][k] * m_b[k][j]
 
     return result
-
