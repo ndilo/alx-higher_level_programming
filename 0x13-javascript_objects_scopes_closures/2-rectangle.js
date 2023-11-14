8bb71a773aa7b65598b87a0793534f857859174a
Brian Ndilo <b_munguti@yahoo.com>
2023-11-14 01:57:17 -0800

    past

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -5,11 +5,7 @@ class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
-      // Create an empty object if conditions are not met
-      Object.create(null);
     }
   }
 }
-
 module.exports = Rectangle;
