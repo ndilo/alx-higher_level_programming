1e77897632370bc7f568032a0d6f851a97878b76
Brian Ndilo <b_munguti@yahoo.com>
2023-11-30 10:14:15 -0800

    0x10. Python - Network 0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-# Send a request to catch_me and make the server respond with "You got me!"
-curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
+# script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -d "user_id=98" -H "Origin:School"
