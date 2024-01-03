5e2b5c293b12eeff19b0a2858f654470f56971eb
brian ndilo <b_munguti@yahoo.com>
2024-01-03 10:29:05 -0800

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,21 @@
 #!/usr/bin/node
-// Read from file
 
-const filesys = require('fs');
-filesys.readFile(process.argv[2], 'utf-8',
-  function (err, data) {
-    if (err) {
-      console.log(err);
-      return;
-    }
+const fs = require('fs');
+
+// Check if the file path argument is provided
+if (process.argv.length < 3) {
+  console.error('Usage: node read-file.js <file-path>');
+  process.exit(1);
+}
+
+// Get the file path from command line arguments
+const filePath = process.argv[2];
+
+// Read the file in UTF-8 encoding
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
     console.log(data);
-  });
+  }
+});
